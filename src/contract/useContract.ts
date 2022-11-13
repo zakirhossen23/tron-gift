27125ba948087c70d74ef938465e5846af3450f0
vybz.live <73053667+zakirhossen23@users.noreply.github.com>
2022-11-13 07:46:53 +0600

    new data

--- src/contract/useContract.ts
+++ src/contract/useContract.ts
@@ -4,7 +4,7 @@ export default async function callContract() {
     const contract = { contract: null, signerAddress: null };
     try {
         if (window?.tronWeb?.defaultAddress?.base58 != false && window?.tronWeb?.defaultAddress?.base58 != undefined && window?.tronLink?.tronWeb !== false){
-            contract.contract =  await window?.tronWeb.contract().at('TTq3H3CkSV1wdbaBwHcEq7qxTXuoSWx8JN');
+            contract.contract =  await window?.tronWeb.contract().at('TVdHdSQLmHnSXnGwZcUWRP2D75TjPKBhEf');
             contract.signerAddress =  window?.tronWeb.defaultAddress.base58;
         }
 
